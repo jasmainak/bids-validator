22bde706e0b276048017d67cfb32f80fbeed71cb
Mainak Jas <mainakjas@gmail.com>
2017-10-13 15:40:48 +0200

    Add test for tsv

--- utils/issues/list.js
+++ utils/issues/list.js
@@ -335,6 +335,6 @@ module.exports = {
     67: {
         key: 'CHANNELS_COLUMN_TYPE',
         severity: 'error',
-        reason:   "Second column of the events file must be named 'type'"
+        reason:   "Second column of the channels file must be named 'type'"
     }
 };
